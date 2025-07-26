6d6f83bee46f5f42ccc5325c7b26dcf683f5ad36
解哲辉 <14881796+xie-zhehui@user.noreply.gitee.com>
2025-07-26 15:25:36 +0800

    阿里云OSS

---
+++ src/main/java/com/xielaoban/aiagent/utils/AliyunOSSProperties.java
@@ -0,0 +1,14 @@
+package com.xielaoban.aiagent.utils;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "aliyun.oss")
+public class AliyunOSSProperties {
+    private String endpoint;
+    private String bucketName;
+    private String region;
+}
